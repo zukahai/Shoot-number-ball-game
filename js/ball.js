ad55c6dbe76f47ea9a081065baa9bf30c7b0d7dc
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-07-14 02:12:59 +0700

    Update ball.js

--- js/ball.js
+++ js/ball.js
@@ -19,17 +19,20 @@ class ball{
         if (this.dy == 0)
             this.dy = this.dx;
         if (this.start) {
-            if (this.x + dx - rCircle <= 0 || this.x + dx + rCircle >= game_W)
+            if (this.x + dx - rCircle < 0 || this.x + dx + rCircle > game_W)
                 this.dx = -this.dx;
-            if (this.y + dy - rCircle <= 0)
+            if (this.y + dy - rCircle < 0)
                 this.dy = -this.dy;
                 
             if (this.y == YYY) {
-                if (this.x <= rCircle)
-                this.x = rCircle;
-                if (this.x >= game_W - rCircle)
+                if (this.x < rCircle)
+                    this.x = rCircle;
+                if (this.x > game_W - rCircle)
                     this.x = game_W - rCircle;
             }
+
+            if (this.y + rCircle < 0)
+                this.y = 3 * rCircle;
             
             for (let i = 0; i < M; i++)
                 for (let j = 0; j < N; j++) 
